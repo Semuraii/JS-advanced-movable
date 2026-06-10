ac0a112d4834b8d91857e025fd96b73e696d179e
Semuraii <208602227+Semuraii@users.noreply.github.com>
2026-06-10 10:10:15 +0200

    moveBall function added

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const ball = document.getElementById('ball');
 const container = document.getElementById('container');
-console.log(container);
+
+let x = 0;
+let y = 0;
+const speed = 5;
+
+function moveBall() {
+    x += speed;
+    y += speed;
+}
 
